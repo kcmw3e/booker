dbadd2208aaa041d925e515f15c7a04e179a4191
kc <kcmw3e@gmail.com>
2022-11-25 04:08:53 -0500

    updated flags to use enums and be better

--- include/booker_flags.h
+++ include/booker_flags.h
@@ -7,7 +7,32 @@
 
 #define FLAGCHAR '-'
 
-bool isflag(const char* arg);
-bool isflagsupported(char flag);
+enum flag {
+    FLAG_NOT_FLAG           =  -1,
+    FLAG_NOT_SUPPORTED      = 0x00,
+    FLAG_HELP               = 0x01,
+    FLAG_OUTPUT_FILE        = 0x02,
+    FLAG_OUTPUT_FORMAT      = 0x04,
+    FLAG_OUTPUT_DECORATIONS = 0x08,
+};
+typedef enum flag flag_t;
+
+enum flagchar {
+    FLAGCHAR_IDENTIFIER         = '-',
+    FLAGCHAR_HELP               = 'h',
+    FLAGCHAR_OUTPUT_FILE        = 'o',
+    FLAGCHAR_OUTPUT_FORMAT      = 'f',
+    FLAGCHAR_OUTPUT_DECORATIONS = 'd',
+};
+typedef enum flagchar flagchar_t;
+
+static const char FLAGCHAR_LONG_HELP[] = "help";
+static const char FLAGCHAR_LONG_OUTPUT_FILE[] = "out";
+static const char FLAGCHAR_LONG_OUTPUT_FORMAT[] = "format";
+static const char FLAGCHAR_LONG_OUTPUT_DECORATIONS[] = "decorations";
+
+flag_t str_to_flag(const char* str);
+bool is_str_flag(const char* str);
+bool isflag(flag_t flag);
 
 #endif // BOOKER_FLAGS_H
--- src/booker_flags.c
+++ src/booker_flags.c
@@ -1,25 +1,53 @@
 #include "booker_flags.h"
 
-static const char FLAGS[] = {
-    'f',
-    'o',
-    'd',
-};
-static const size_t NFLAGS = sizeof(FLAGS)/sizeof(*FLAGS);
+flag_t match_flag(char c);
+flag_t match_flag_long(const char* s);
 
-bool isflag(const char* arg) {
-    // a flag has a single FLAGCHAR character (usually '-') and a single letter after it
-    size_t len = strlen(arg);
-    if (len != 2) return false;
-    if (arg[0] != FLAGCHAR) return false;
+flag_t str_to_flag(const char* str) {
+    size_t len = strlen(str);
+    if (len < 2) return FLAG_NOT_FLAG;
+    if (str[0] != FLAGCHAR_IDENTIFIER) return FLAG_NOT_FLAG;
+    if (len == 2) return match_flag(str[1]); // ignore first '-' char
+    if (str[1] == FLAGCHAR_IDENTIFIER) return match_flag_long(str + 2); // +2 to ignore leading '--'
+    return FLAG_NOT_FLAG;
+}
 
+bool is_str_flag(const char* str) {
+    flag_t flag = str_to_flag(str);
+    if (flag == FLAG_NOT_FLAG) return false;
     return true;
 }
 
-bool isflagsupported(char flag) {
-    for (size_t i = 0; i < NFLAGS; i++) {
-        if (flag == FLAGS[i]) return true;
+bool isflag(flag_t flag) {
+    switch(flag) {
+        case (FLAG_HELP):
+        case (FLAG_OUTPUT_FILE):
+        case (FLAG_OUTPUT_FORMAT):
+        case (FLAG_OUTPUT_DECORATIONS):
+            return true;
+        default:
+            break;
     }
+    return false;
+}
 
-        return false;
+flag_t match_flag(char c) {
+    switch (c) {
+        case (FLAGCHAR_HELP):               return FLAG_HELP;
+        case (FLAGCHAR_OUTPUT_FILE):        return FLAG_OUTPUT_FILE;
+        case (FLAGCHAR_OUTPUT_FORMAT):      return FLAG_OUTPUT_FORMAT;
+        case (FLAGCHAR_OUTPUT_DECORATIONS): return FLAG_OUTPUT_DECORATIONS;
+        default:
+            break;
+    }
+        return FLAG_NOT_SUPPORTED;
+}
+
+flag_t match_flag_long(const char* flag) {
+    // flag should not include the leading '--'
+    if (strcmp(flag, FLAGCHAR_LONG_HELP) == 0)               return FLAG_HELP;
+    if (strcmp(flag, FLAGCHAR_LONG_OUTPUT_FILE) == 0)        return FLAG_OUTPUT_FILE;
+    if (strcmp(flag, FLAGCHAR_LONG_OUTPUT_FORMAT) == 0)      return FLAG_OUTPUT_FORMAT;
+    if (strcmp(flag, FLAGCHAR_LONG_OUTPUT_DECORATIONS) == 0) return FLAG_OUTPUT_DECORATIONS;
+    return FLAG_NOT_SUPPORTED;
 }
