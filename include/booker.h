ac285b877a68d2aba75b4ad6aba8a5ec4ce898e9
kc <kcmw3e@gmail.com>
2022-11-25 04:15:31 -0500

    added booker.h
    at the moment just holds enum for result returns

---
+++ include/booker.h
@@ -0,0 +1,12 @@
+#ifndef BOOKER_H
+#define BOOKER_H
+
+enum result {
+    RESULT_TOO_FEW_ARGS,
+    RESULT_SUCCESS,
+    RESULT_NULL_ARG,
+    RESULT_INVALID_FLAG,
+};
+typedef enum result result_t;
+
+#endif // BOOKER_H
